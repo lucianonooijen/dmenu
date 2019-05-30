6d659207200f026a3c7b1e750b86592aed5ac21b
Luciano Nooijen <luciano@bytecode.nl>
2019-05-30 12:22:14 +0200

    Changed colors to match color scheme, set defaults
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- config.def.h
+++ config.def.h
@@ -4,18 +4,18 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#1b1918" },
+	[SchemeSel] = { "#ffffff", "#ae81ff" },
+	[SchemeOut] = { "#ffffff", "#ae81ff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+static unsigned int lines      = 20;
+static unsigned int lineheight = 28;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
@@ -24,4 +24,4 @@ static unsigned int lineheight = 0;         /* -h option; minimum height of a me
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 4;
